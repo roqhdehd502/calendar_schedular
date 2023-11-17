1922fa3d8a5061aa2b48b2efff8056bbabb5656e
MinwooNa <mwna@spacecl.co.kr>
2023-11-17 13:17:12 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:calendar_schedular/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      home: HomeScreen(),
+    ),
+  );
+}
