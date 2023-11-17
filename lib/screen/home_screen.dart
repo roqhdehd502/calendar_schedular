726c6de67c46e0c9dbaea14bd8af96bfa0243035
MinwooNa <mwna@spacecl.co.kr>
2023-11-17 15:14:51 +0900

    main: 스케쥴 Card ListView 세팅

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -35,13 +35,23 @@ class _HomeScreenState extends State<HomeScreen> {
               scheduleCount: 3,
             ),
             const SizedBox(height: 8.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ScheduleCard(
-                startTime: 8,
-                endTime: 14,
-                content: '프로그래밍 공부하기',
-                color: Colors.red,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListView.separated(
+                  itemCount: 6,
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(height: 8.0);
+                  },
+                  itemBuilder: (context, index) {
+                    return ScheduleCard(
+                      startTime: 8,
+                      endTime: 14,
+                      content: '프로그래밍 공부하기',
+                      color: Colors.red,
+                    );
+                  },
+                ),
               ),
             ),
           ],
