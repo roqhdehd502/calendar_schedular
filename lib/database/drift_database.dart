ae45d3158bde1a1c613d7d4bb2029419018da3d0
MinwooNa <natorque5552@gmail.com>
2023-11-18 18:58:08 +0900

    main: home_screen 등록한 스케줄러 불러오기 기능 수정 - 3

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -31,6 +31,9 @@ class LocalDatabase extends _$LocalDatabase {
       innerJoin(categoryColors, categoryColors.id.equalsExp(schedules.colorId))
     ]);
     query.where(schedules.date.equals(date));
+    query.orderBy([
+      OrderingTerm.asc(schedules.startTime),
+    ]);
     return query.watch().map(
           (rows) => rows
               .map(
