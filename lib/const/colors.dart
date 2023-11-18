9ab87f332d406ff6210984bfb9528e6f828392bd
MinwooNa <natorque5552@gmail.com>
2023-11-18 16:02:34 +0900

    main: main 화면 DB 연동

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
 const PRIMARY_COLOR = Color(0xFF0DBEBE);
+const DEFAULT_COLORS = [
+  // 빨
+  'F44336',
+  // 주
+  'FF9800',
+  // 노
+  'FFFB3B',
+  // 초
+  'FCAF50',
+  // 파
+  '2196F3',
+  // 남
+  '3F51B5',
+  // 보
+  '9C27B0',
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
+import 'package:calendar_schedular/database/drift_database.dart';
 import 'package:calendar_schedular/screen/home_screen.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+import 'const/colors.dart';
 
 void main() async {
   // 위젯 실행전 초기화 여부 함수
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = LocalDatabase();
+  final colors = await database.getCategoryColors();
+
+  if (colors.isEmpty) {
+    for (String hexCode in DEFAULT_COLORS) {
+      await database.createCategoryColor(
+        CategoryColorsCompanion(
+          hexCode: Value(hexCode),
+        ),
+      );
+    }
+  }
 
   runApp(
     MaterialApp(
