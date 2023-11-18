b068994200e17b2dbbd02fd99163fe293708c787
MinwooNa <natorque5552@gmail.com>
2023-11-18 18:15:20 +0900

    main: home_screen 등록한 스케줄러 불러오기 기능 수정 - 1

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -24,8 +24,14 @@ class LocalDatabase extends _$LocalDatabase {
   @override
   int get schemaVersion => 1;
 
-  // 스케쥴 목록 Select (업데이트 된 값을 불러와야 하므로 Stream, watch 사용)
-  Stream<List<Schedule>> watchSchedules() => select(schedules).watch();
+  // 일자별 스케쥴 목록 Select (업데이트 된 값을 불러와야 하므로 Stream, watch 사용)
+  Stream<List<Schedule>> watchSchedules(DateTime date) {
+    // final query = select(schedules);
+    // query.where((tbl) => tbl.date.equals(date));
+    // return query.watch();
+    return (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
+  }
+
   // 카테고리 컬러 테이블 Select
   Future<List<CategoryColor>> getCategoryColors() =>
       select(categoryColors).get();
